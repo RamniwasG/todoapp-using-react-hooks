6f6e21bb910308cd5207a311736599e7ef95b099
Ramniwas Gupta <rgupta@QINDM034.local>
2019-09-20 17:38:12 +0530

    initial setup, add/edit/delete gredient item/component done, usage firebase backend setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import TodoUsingHooks from './components/TodoUsingHooks';
 
 function App() {
   return (
     <div className="App">
+      <div className="app-heading">Ingredients App using React Hooks</div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TodoUsingHooks />
       </header>
     </div>
   );
---
+++ src/components/IngredientList.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import Loader from '../common/Loader';
+
+const IngredientList = (props) => {
+
+    const [selectedIndex, setSelectedIndex] = useState(-1)
+
+    const handleEdit = (id, index) => {
+        setSelectedIndex(index)
+        props.onEdit(id)
+    }
+
+    const handleDelete = (id) => {
+        props.onDelete(id)
+    }
+
+    return <React.Fragment>
+        <h4 className="text-center">Ingredients List</h4>
+        <ul className="list-group">
+            { props.isLoading && <Loader /> }
+            { !props.isLoading && props.ingredients.length === 0 &&
+                <li className="list-group-item text-center">No Ingredient Added Yet!</li>
+            }
+            { !props.isLoading && props.ingredients.length > 0 && props.ingredients.map( (ingredient, index) => {
+                return (
+                    <li 
+                        key={ingredient.id} 
+                        className="list-group-item" 
+                        style = {{
+                            display:'inline-flex', 
+                            backgroundColor: selectedIndex === index ? 'lightgrey' : 'white'
+                        }}>
+                        <div className="col-sm-5 text-left">{ingredient.name}</div>
+                        <div className="col-sm-3"><b>{ingredient.amount}$</b></div>
+                        <div className="col-sm-4" style={{display: 'inline-flex'}}>
+                            <button
+                                type="button"
+                                className="btn btn-success btn-sm m-r-sm"
+                                onClick={() => handleEdit(ingredient, index)}>Edit</button>
+                            <button 
+                                type="button" 
+                                className="btn btn-danger btn-sm" 
+                                onClick={() => handleDelete(ingredient.id)}>Delete</button>
+                        </div>
+                    </li>
+                )
+            })}
+        </ul>
+    </React.Fragment>
+}
+
+export default IngredientList;
